7db5409e7f578407513a5fe789eba8fbb3d7d2e5
RUSHIKESHCHEEDALLA <139362278+RUSHIKESHCHEEDALLA@users.noreply.github.com>
2025-07-07 17:52:46 +0000

    JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./Chai"
 function App() {
+  const username="rushikesh"
   return (
     <>
     <Chai/>
     <h1>ok comming wait!</h1>
+    <p>hai hello {username}</p>
     </>
   )
 }
-
 export default App;
